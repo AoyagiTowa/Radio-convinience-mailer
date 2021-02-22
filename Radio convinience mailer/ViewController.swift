5bca294231c904dc97623462e1f5139eb2d392ad
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-22 17:09:11 +0900

    Initial Commit

---
+++ Radio convinience mailer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Radio convinience mailer
+//
+//  Created by Towa Aoyagi on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
