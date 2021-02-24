da1ef45357b16972bd2c9eb777356904cd089a8f
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-24 17:20:17 +0900

    Create all app ViewController and Update addRadioInformation View

---
+++ Radio convinience mailer/RadioClass.swift
@@ -0,0 +1,24 @@
+//
+//  RadioClass.swift
+//  Radio convinience mailer
+//
+//  Created by Towa Aoyagi on 2021/02/24.
+//
+
+import UIKit
+
+class RadioClass {
+    var radioName: String
+    var radioAddress: String
+    var radioDays: Array<Bool> = Array(repeating: false, count: 7)
+    var radioStart: Date
+    var radioStop: Date
+    
+    init(radioName: String,radioAddress: String,radioDays: [Bool], radioStart: Date,radioStop: Date) {
+        self.radioName = radioName
+        self.radioAddress = radioAddress
+        self.radioDays = radioDays
+        self.radioStart = radioStart
+        self.radioStop = radioStop
+    }
+}
