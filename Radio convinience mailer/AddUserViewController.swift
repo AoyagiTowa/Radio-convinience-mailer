9ace3f2a54aa183ef2409cebbb89b6ed30e41695
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-24 22:16:05 +0900

    Create storyboard

--- Radio convinience mailer/AddUserViewController.swift
+++ Radio convinience mailer/AddUserViewController.swift
@@ -7,23 +7,74 @@
 
 import UIKit
 
-class AddUserViewController: UIViewController {
-
+class AddUserViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet var jenderField: UITextField!
+    var pickerView: UIPickerView = UIPickerView()
+    
+    let jenderList = ["男性","女性","非公開"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.showsSelectionIndicator = true
+        
+        let toolbar = UIToolbar(frame: CGRectMake(0, 0, 0, 35))
+        _ = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(AddUserViewController.done))
+        
+        self.jenderField.inputView = pickerView
+        self.jenderField.inputAccessoryView = toolbar
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return jenderList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return jenderList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.jenderField.text = jenderList[row]
+    }
+    
+    func cancel() {
+        self.jenderField.text = ""
+        self.jenderField.endEditing(true)
+    }
+    
+    @objc func done() {
+        self.jenderField.endEditing(true)
+    }
+    
+    
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
