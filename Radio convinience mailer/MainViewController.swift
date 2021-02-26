4777bc820693e3b7cd1dbb3d669fb511c4238ce2
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-26 11:35:19 +0900

    add mainView

--- Radio convinience mailer/MainViewController.swift
+++ Radio convinience mailer/MainViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet var table: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
     
 
