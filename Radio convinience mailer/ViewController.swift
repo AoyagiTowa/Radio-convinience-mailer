13736a4b27cc0b13af1c9296a7ac81ebb1de6cfa
TowaAoyagi <tab.git.kamui@gmail.com>
2021-03-01 01:00:52 +0900

    merge master

--- Radio convinience mailer/ViewController.swift
+++ Radio convinience mailer/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var senderAdressField: UITextField!
+    @IBOutlet var mainTitleField: UITextField!
+    @IBOutlet var contentField: UITextField!
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func send(){
+        
     }
 
 
